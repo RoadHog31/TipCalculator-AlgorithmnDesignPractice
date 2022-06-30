4c378b422eeec77c36e4e02411d6f045524c51bf
Your Name <you@example.com>
2022-06-30 21:52:23 +0100

    Comments added. Test with console log in terminal

---
+++ main.js
@@ -0,0 +1,18 @@
+// TipCalculator:
+
+// Initialize billAmount to 0
+// Initialize tip to 0
+// Initialize tipRate to 0
+// Initialize total to 0
+// Prompt for billAmount with "What is the bill amount?"
+// Prompt for tipRate with "What is the tip rate?"
+// convert billAmount to a number
+// convert tipRate to a number
+// tip = billAmount * (tipRate / 100)
+// round tip up to nearest cent
+// total = billAmount + tip
+// Display "Tip: $" + tip
+// Display "Total: $" + total
+// End
+
+console.log("Hi!");
